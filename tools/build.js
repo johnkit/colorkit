8b009cbf3a661407da0b3ea83c43f4e08dcbdc8d
John Tourtellott <john.tourtellott@kitware.com>
2018-07-17 15:33:21 -0400

    Install rollup & babel; now builds es, cjs, and umd packages
    * Moves source to src/ folder
    * Builds to dist/ folder
    * Note the "module" entry added to package.json

---
+++ tools/build.js
@@ -0,0 +1,72 @@
+/*
+ * Adapted from:
+ * Babel Starter Kit (https://www.kriasoft.com/babel-starter-kit)
+ *
+ * Copyright © 2015-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license:
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+*/
+
+'use strict';
+
+const fs = require('fs');
+const del = require('del');
+const rollup = require('rollup');
+const babel = require('rollup-plugin-babel');
+const pkg = require('../package.json');
+
+let promise = Promise.resolve();
+
+// Clean up the output directory
+promise = promise.then(() => del(['dist/*']));
+
+// Compile source code into a distributable format with Babel
+['es', 'cjs', 'umd'].forEach((format) => {
+  promise = promise.then(() => rollup.rollup({
+    input: 'src/index.js',
+    external: Object.keys(pkg.dependencies),
+    plugins: [babel(Object.assign(pkg.babel, {
+      babelrc: false,
+      exclude: 'node_modules/**',
+      runtimeHelpers: true,
+      presets: pkg.babel.presets.map(x => (x === 'latest' ? ['latest', { es2015: { modules: false } }] : x)),
+    }))],
+  }).then(bundle => bundle.write({
+    file: `${format === 'cjs' ? 'index' : `index.${format}`}.js`,
+    dir: 'dist',
+    format,
+    sourceMap: true,
+    name: format === 'umd' ? pkg.name : undefined,
+  })));
+});
+
+// Copy package.json and LICENSE.txt
+promise = promise.then(() => {
+  delete pkg.private;
+  delete pkg.devDependencies;
+  delete pkg.scripts;
+  delete pkg.eslintConfig;
+  delete pkg.babel;
+  fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
+  //fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8');  fs.writeFileSync('dist/index.d.ts', fs.readFileSync('src/index.d.ts', 'utf-8'), 'utf-8');
+});
+
+promise.catch(err => console.error(err.stack)); // eslint-disable-line no-console
